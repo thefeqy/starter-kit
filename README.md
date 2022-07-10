5d6f662c27225726602548443802e850b084a8d2
Muhammed Elfeqy <osclancer@gmail.com>
2022-07-10 19:18:22 +0200

    The initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# StarterKit - is a Frontend website folder structure
+
+##### First, install the project required modules by running the following command:
+```
+    npm install
+```
+
+##### to run the development mode, just run the following command:
+```
+    npm run dev
+```
+##### to build the production website, just run the following command:
+```
+    npm run build
+```
+
+By
+- [Muhammed ElFeqy](https://feqy.dev)
---
+++ gulpfile.babel.js
@@ -0,0 +1,99 @@
+'use strict';
+
+import gulp from 'gulp';
+import sassLib from 'sass';
+import yargs from 'yargs';
+import gulpSass from 'gulp-sass';
+import gulpIf from 'gulp-if';
+import sourceMaps from 'gulp-sourcemaps';
+import cleanCss from 'gulp-clean-css';
+import imagemin from 'gulp-imagemin';
+import concat from 'gulp-concat';
+import uglify from 'gulp-uglify';
+import browserSync from 'browser-sync';
+
+const sass = gulpSass(sassLib);
+const sync = browserSync.create();
+
+const PRODUCTION = yargs(process.argv.slice(2)).argv.prod;
+
+const paths = {
+	html: {
+		src: 'src/**/*.html',
+		dest: 'build/',
+	},
+	styles: {
+		src: 'src/assets/scss/*.scss',
+		dest: 'build/assets/css',
+	},
+	scripts: {
+		src: 'src/assets/js/**/*.js',
+		dest: 'build/assets/js',
+	},
+	images: {
+		src: 'src/assets/images/**/*.{jpg,jpeg,png,gif,svg}',
+		dest: 'build/assets/images',
+	},
+};
+
+// Copy HTML files
+gulp.task('copyHTML', () => {
+	return gulp.src(paths.html.src).pipe(gulp.dest(paths.html.dest));
+});
+
+// Building and minifying styles
+gulp.task('buildStyles', () => {
+	return gulp
+		.src(paths.styles.src)
+		.pipe(gulpIf(!PRODUCTION, sourceMaps.init()))
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulpIf(PRODUCTION, cleanCss({ compatibility: 'ie8' })))
+		.pipe(gulpIf(!PRODUCTION, sourceMaps.write()))
+		.pipe(gulp.dest(paths.styles.dest))
+		.pipe(sync.stream());
+});
+
+// Concatenate and minify scripts
+gulp.task('concatScripts', () => {
+	return gulp
+		.src(paths.scripts.src)
+		.pipe(concat('main.js'))
+		.pipe(gulpIf(PRODUCTION, uglify()))
+		.pipe(gulp.dest(paths.scripts.dest))
+		.pipe(sync.stream());
+});
+
+// Optimizing Images
+gulp.task('optimizeImages', () => {
+	return gulp
+		.src(paths.images.src)
+		.pipe(gulpIf(PRODUCTION, imagemin()))
+		.pipe(gulp.dest(paths.images.dest))
+		.pipe(sync.stream());
+});
+
+// Dev
+gulp.task('dev', () => {
+	sync.init({
+		server: {
+			baseDir: './build/',
+		},
+	});
+
+	gulp.watch('src/**/*.html', gulp.series('copyHTML')).on(
+		'change',
+		sync.reload
+	);
+	gulp.watch('src/assets/scss/**/*.scss', gulp.series('buildStyles'));
+	gulp.watch('src/assets/js/**/*.js', gulp.series('concatScripts'));
+	gulp.watch('src/assets/images/*', gulp.series('optimizeImages')).on(
+		'change',
+		sync.reload
+	);
+});
+
+// Build Script - "npm run build"
+gulp.task(
+	'default',
+	gulp.series('copyHTML', 'buildStyles', 'concatScripts', 'optimizeImages')
+);
